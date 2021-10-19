8186edb367fd13513c7d4d9352e283a7db53bb9c
kawser00 <kawsarmahmud358@gmail.com>
2021-10-20 00:37:12 +0600

    [ADD] freecodecamp file update

--- FREECODECAMP/Functional-programming/exercise.js
+++ FREECODECAMP/Functional-programming/exercise.js
@@ -34,3 +34,36 @@ var new_s2 = s.myFilter(function(item) {
   return item % 2 === 1;
 });
 console.log(new_s2);
+
+
+//test 15: filter only integer number and return their squared value
+//================================================================
+const squareList = arr => {
+  return arr.filter(num => num >0 && num % parseInt(num) === 0).map(num => Math.pow(num, 2));
+};
+
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
+
+
+
+//test 21: Fill in the urlSlug function so it converts a string title and 
+//returns the hyphenated version for the URL. You can use any of the methods covered in this section, 
+//and don't use replace. Here are the requirements:
+
+//The input is a string with spaces and title-cased words
+//The output is a string with the spaces between words replaced by a hyphen (-)
+//The output should be all lower-cased letters
+//The output should not have any spaces
+//================================================================
+var globalTitle = "Winter Is Coming";
+
+function urlSlug(title) {
+  return title
+    .split(" ")
+    .filter(substr => substr !== "")
+    .join("-")
+    .toLowerCase();
+}
+var winterComing = urlSlug(globalTitle); // Should be "winter-is-coming"
+console.log(winterComing);
